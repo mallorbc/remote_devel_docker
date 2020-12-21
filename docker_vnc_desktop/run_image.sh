8288910b0ead0948fe5411b897086d3a6cd45994
Blake Mallory <blakecmallory@gmail.com>
2020-12-21 01:20:37 -0500

    made it so only device 0 is added to docker container

--- docker_vnc_desktop/run_image.sh
+++ docker_vnc_desktop/run_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
 dir_to_mount=$(pwd)
-docker run --gpus all -d -p 5901:5901 -p 6901:6901 -v $dir_to_mount:/shared_drive docker_desktop_pytorch
+docker run --gpus device=0 -d -p 5901:5901 -p 6901:6901 -v $dir_to_mount:/shared_drive docker_desktop_pytorch
